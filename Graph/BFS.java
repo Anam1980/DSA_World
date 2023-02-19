2aca3eb18427b8c0cadee24480313e99b2130d88
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-02-19 11:21:22 +0530

    Create BFS.java

---
+++ Graph/BFS.java
@@ -0,0 +1,64 @@
+//BFS Implementation>>>>>
+//Given a directed graph. The task is to do Breadth First Search of this graph.
+//Note: Graph does not contain multiple edges and self loops.
+
+//Input Format
+//The First line of test case contains two integers N and E which denotes the no of vertices and no of edges respectively.
+
+//The Second line of test case contains E space separated pairs u and v denoting that there is a edge from u to v .
+
+import java.util.*;
+import java.io.*;
+ 
+class Graph {
+    public int vertices;
+    public ArrayList<Integer>[] adjList;
+ 
+    Graph(int v) {
+        this.vertices = v+1;
+        adjList = new ArrayList[v+1];
+        
+        for (int i = 0; i <= v; i++) adjList[i] = new ArrayList<Integer>();
+    }
+ 
+    void addEdge(int v, int w) {
+        adjList[v].add(w);
+     
+    }
+ 
+    void BFS(int x) {
+         boolean[] visited = new boolean[vertices];
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        
+        visited[x] = true;
+        queue.add(x);
+        
+        while(queue.size() != 0) {
+            x = queue.poll();
+            System.out.print(x + " ");
+            
+            for(int i : adjList[x]) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
+ 
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int e = sc.nextInt();
+        Graph g = new Graph(110);
+        for(int i =0;i<e;i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            g.addEdge(x,y);
+        }
+        g.BFS(0);
+    }
+}
