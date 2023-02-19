eb47de9a1cdc3d40f04101df926cb9c71521718b
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-02-19 17:40:52 +0530

    Update and rename Inset a Node.java to Insert a Node.java

--- Binary Search Tree/Insert a Node.java
+++ Binary Search Tree/Insert a Node.java
@@ -44,21 +44,24 @@ class BST{
 }
 
 class Solution{
+	//TC:O(N) SC:O(h)
     public Node insertNode(Node root, int val){
 
+	    //create a node for val
 		 Node nn=new Node(val);
 
+	    //if root is null , default the first node is new one
 		if(root==null)return nn;
-	
+	//if given val less than root data then insert node in left
        if(val<root.val){
-		  Node nlft=insertNode(root.left, val);
-		   root.left=nlft;
-		   
+	       Node nlft=insertNode(root.left, val);
+	       root.left=nlft;	   
 	   }
-		else if(val>root.val){
-			Node nrgt=insertNode(root.right, val);
-			root.right=nrgt;
-		}
+	    ////if given val greater than root data then insert node in left
+	else if(val>root.val){
+		Node nrgt=insertNode(root.right, val);
+		root.right=nrgt;
+	}
 
 		return root;
     }
