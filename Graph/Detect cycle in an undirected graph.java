3fb9f230954f017491031fd1cff6981302490878
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-02-20 14:17:34 +0530

    Update Detect cycle in an undirected graph.java

--- Graph/Detect cycle in an undirected graph.java
+++ Graph/Detect cycle in an undirected graph.java
@@ -11,32 +11,30 @@ class Solution {
   
     public static boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
 
-		boolean[] visited=new boolean[V];
-		for(int i=0; i<V; i++){
-			if(visited[i]==true)continue;//if i is already visited, it must have been covered in prevous components
+	boolean[] visited=new boolean[V];
+	for(int i=0; i<V; i++){
+        if(visited[i]==true)continue;//if i is already visited, it must have been covered in prevous components
 
-			Queue<Integer>que=new LinkedList<>();
-			que.add(i);
+		Queue<Integer>que=new LinkedList<>();
+		que.add(i);
 
-			while(que.size()>0){
-				//remove
-				int curr=que.poll();
+		while(que.size()>0){
+			//remove
+			int curr=que.poll();
+			if(visited[curr]==true)return true;// already visited and again trying to visit therefore cycle detecte
+			//mark visited if not already visited	
+			visited[curr]=true;
 
-				if(visited[curr]==true)return true;// already visited and again trying to visit therefore cycle detected
-				
-				//mark visited if not already visited	
-				visited[curr]=true;
-
-				//work*
-				//add not visited
-				for(int nbr : adj.get(curr)){
-					if(!visited[nbr]){
-						//add unvisited nbr
-						que.add(nbr);
-					}
-				}
-			}		
+			//work*
+			//add not visited
+			for(int nbr : adj.get(curr)){
+				if(!visited[nbr]){
+				//add unvisited nbr
+				que.add(nbr);
+			}
 		}
+	    }		
+        }
 		return false;
     }
 }
@@ -44,17 +42,17 @@ class Solution {
 //----------------------------DFS APPROACH-------------------------------------------------------//
  public static boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
 
-		boolean[] visited=new boolean[V];
-		for(int i=0; i<V; i++){
-			if(visited[i]==true)continue;//if i is already visited, it must have been covered in prevous components
+     boolean[] visited=new boolean[V];
+     for(int i=0; i<V; i++){
+	if(visited[i]==true)continue;//if i is already visited, it must have been covered in prevous components
       
       //here is the logic
       //we go to traverse in depth by storing the parent also ,
       //if any visited node during traversal from given start  has not same value as parent that means it is a cycle from that node
-				if(hasCycle(i, visited, -1, adj){
+     if(hasCycle(i, visited, -1, adj){
           return true;
-		}
-		return false;
+	}
+     return false;
   }
            
    static boolean hasCycle(int v, boolean visited[], int parent, ArrayList<ArrayList<Integer>> adj){
