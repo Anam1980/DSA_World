e9259dc1ce982c6d3dd52e1e88d05364f44ccc97
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-02-22 13:31:00 +0530

    Update PerfectStudents.java

--- Graph/Components/PerfectStudents.java
+++ Graph/Components/PerfectStudents.java
@@ -1,3 +1,10 @@
+//You are given a number n (representing the number of students). Each student will have an id from 0 to n - 1. 
+//You are given a number k (representing the number of clubs).
+//In the next k lines, two numbers are given separated by a space.
+//The numbers are ids of students belonging to same club. 
+//You have to find in how many ways can we select a pair of students such that both students are from different clubs.
+
+
 import java.io.*;
 import java.util.*;
   class Edge {
