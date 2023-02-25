45b1c6fbad95aba90e10559aa7f7a487471c048f
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-02-25 09:39:30 +0530

    Create Number of set bits.java

---
+++ Bit Manipulation/Number of set bits.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int setBits(int n) {
+       int cnt=0;
+		while(n>0){
+			//if it is set then cnt
+			if((n & 1)==1)cnt++;
+			//shift by 1 
+			n>>=1;
+		}
+
+		return cnt;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Solution Obj = new Solution();
+        sc.close();
+        System.out.println(Obj.setBits(n));
+    }
+}
