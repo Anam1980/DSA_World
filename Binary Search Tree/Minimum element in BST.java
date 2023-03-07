06991a315aa3c2d00bf60418c90d1f5a7a594e9d
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-03-07 18:34:08 +0530

    Create Minimum element in BST.java

---
+++ Binary Search Tree/Minimum element in BST.java
@@ -0,0 +1,75 @@
+/*You are given N nodes and have to form BST from it. The task is to find the minimum element in this BST.*/
+
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int val;
+    Node left, right;
+    Node(int val){
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+class BST{
+    Node root = null;
+    BST(){
+        
+    }
+    BST(Node root){
+        root=root;
+    }
+    Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.val == val)
+            return root;
+        if(val < root.val){
+            root.left = insert(root.left, val);
+        }else{
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+    void print(Node root){
+        System.out.print(root.val + " ");
+        if(root.left != null){
+            print(root.left);
+        }
+        if(root.right != null){
+            print(root.right);
+        }
+    }
+}
+
+class Solution{
+    public int minimumNode(Node root){
+        //min alswys in left part of bst 
+		//min of tree is leaf of left
+
+		if(root.left!=null){
+			return minimumNode(root.left);
+		}
+		else{
+			return root.val;
+		}
+    }
+}
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        BST t = new BST();
+        for(int i = 0; i < n; i++){
+            t.root = t.insert(t.root, input.nextInt());
+        }
+        Solution obj=new Solution();
+        int result= obj.minimumNode(t.root);
+        System.out.print(result);
+        
+    }
+}
