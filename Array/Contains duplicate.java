32a39f67ad82cfe9fec58ec75adfd13a3d7aa529
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-03-10 23:45:19 +0530

    Easy

--- Array/Contains duplicate.java
+++ Array/Contains duplicate.java
@@ -1,6 +1,7 @@
 /*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.*/
 
 class Solution {
+    
     public boolean containsDuplicate(int[] nums) {
         //create hashmap to store freq as a value
         Map<Integer, Integer>hm=new HashMap<>();
