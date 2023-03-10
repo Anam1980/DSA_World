81e2d7ec2b56ad9546c3a3b60766206253a91a6a
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-03-10 14:41:46 +0530

    Create Chocolate Distribution.java

---
+++ Array/Chocolate Distribution.java
@@ -0,0 +1,30 @@
+/*Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. 
+Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.*/
+
+class Solution
+{
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+       //sort the list
+       Collections.sort(a);
+       
+       //overall min
+       long minAll = a.get(n-1);
+       
+       for(int i=0; i<n-m+1; i++){
+           //first->min
+           //last->max
+           int min=a.get(i);
+           int max=a.get(i+m-1);
+           
+           //max-min 
+           minAll=Math.min(minAll, (max-min));
+       }
+       
+       return minAll;
+    }
+}
+
+//TC-O(N) SC-O(1)
