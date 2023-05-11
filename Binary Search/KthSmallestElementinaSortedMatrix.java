980f2c1175bd41eff66bf19e7d666a1469dfbe37
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-05-11 21:52:50 +0530

    Medium

--- Binary Search/KthSmallestElementinaSortedMatrix.java
+++ Binary Search/KthSmallestElementinaSortedMatrix.java
@@ -4,7 +4,7 @@ class Solution {
         int st=matrix[0][0];
         int end=matrix[n-1][n-1];
 
-        int ans=Integer.MIN_VALUE;
+       int ans=Integer.MIN_VALUE;
 
         while(st<=end){
             int mid=(st+end)/2;
