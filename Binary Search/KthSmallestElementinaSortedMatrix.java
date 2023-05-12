6e7544615eddd5799a1f466d7f63970aa576d3f6
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-05-12 16:03:09 +0530

    Medium

--- Binary Search/KthSmallestElementinaSortedMatrix.java
+++ Binary Search/KthSmallestElementinaSortedMatrix.java
@@ -1,10 +1,12 @@
+import java.util.*;
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n= matrix.length;
         int st=matrix[0][0];
         int end=matrix[n-1][n-1];
 
-       int ans=Integer.MIN_VALUE;
+        int ans=Integer.MIN_VALUE;
 
         while(st<=end){
             int mid=(st+end)/2;
@@ -13,27 +15,45 @@ class Solution {
 
             int count=0;
 
-            while(i<=n-1 && j>=0){
-                //count the number of elemnet less than or equal to mid value
-                if(matrix[i][j]<=mid){
-                    count+=j+1;
-                    i++;
-                }
-                else{
+            while(i<=n-1){
+				// count number of elements less than or equal to mid
+                while( j>=0 && matrix[i][j]>mid){
+                    
                     j--;
                 }
+                count+=j+1;
+				i++;
             }
-
-            //update the range of ans value
+              // Narrow down the range based on the count of elements less than or equal to middle
             if(count<k){
-                st=mid+1;
+                st=mid+1;	
             }
             else{
                 end=mid-1;
-                ans=mid;
+                
             }
         }
 
-        return ans;
+        return st;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] matrix = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                matrix[i][j] = sc.nextInt();
+        }
+        
+        int k = sc.nextInt();
+        
+        Solution obj = new Solution();
+        System.out.println(obj.kthSmallest(matrix, k));
+        
+        sc.close();
     }
 }
