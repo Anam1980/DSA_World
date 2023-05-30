525c89cd72ddc26a0642a5758641929b29c9915d
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-05-30 14:41:12 +0530

    Medium

---
+++ Binary Tree/Maximum Product of Splitted Binary Tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int MOD = (int)(1e9 + 7);
+    int nodesSum;
+    long ans;
+     public int maxProduct(TreeNode root) {
+         nodesSum = sum(root); 
+         ans = 0l;
+         MaxProduct(root);
+         return (int)(ans % MOD);
+     }
+     public int sum(TreeNode node){
+         if(node == null) return 0;
+         return sum(node.left) + sum(node.right) + node.val;
+     }
+     public int MaxProduct(TreeNode node){
+         if(node == null) return 0;
+         int substree1Sum = MaxProduct(node.left) + MaxProduct(node.right)+ node.val;
+         int substree2Sum = nodesSum - substree1Sum;
+         ans = Math.max(ans, (long)substree1Sum * substree2Sum);
+         return substree1Sum;
+     }
+}
