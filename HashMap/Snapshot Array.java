99cd5a226d528215197ca8f5c2ac6f4a21ddaeae
Anam Sheikh <113501770+Anam1980@users.noreply.github.com>
2023-10-27 16:31:47 +0530

    Create Snapshot Array.java

---
+++ HashMap/Snapshot Array.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class Main
+{
+	static HashMap<Integer, Integer>[]mapArr;
+	static int snapId;
+	
+    public static void SnapshotArray(int length) {
+		mapArr=new HashMap[length];
+		snapId=0;
+    }
+
+    public static void set(int index, int val) {
+		if(mapArr[index]==null){
+			mapArr[index]=new HashMap<>();
+		}
+
+		mapArr[index].put(snapId, val);
+
+    }
+
+    public static int snap() {
+
+		snapId++;
+
+		return snapId-1;
+    }
+
+    public static int get(int index, int snap_id) {
+		if(mapArr[index]==null)return 0;
+
+		//find the last updated val on that index 
+		while(snap_id>=0 && mapArr[index].containsKey(snap_id)==false){
+			snap_id--;
+		}
+
+		if(snap_id==-1)return 0;
+
+		return mapArr[index].get(snap_id);
+    }
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		String temp;
+		int param1, param2;
+		param1 = input.nextInt();
+        SnapshotArray(param1);
+        String[] arr = new String[n];
+		for (int i = 0; i < n; i++){
+
+            temp = input.next();
+		    if(temp.equals("set") || temp.equals("get"))
+            {
+              param1 = input.nextInt();
+              param2 = input.nextInt();
+              if(temp.equals("set"))
+              {
+                  set(param1, param2);
+              }
+              else{
+                  System.out.println(get(param1, param2));
+              }
+            }
+            else{
+                System.out.println(snap());
+            }
+		}
+        
+	}
+}
